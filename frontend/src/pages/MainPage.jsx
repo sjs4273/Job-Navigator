ea530471a5bbde64b6e8e6aec20562c837552292
sonhyunsoo715 <thsgustn0715@gmail.com>
2025-06-25 11:13:55 +0900

    refactor: 이미지 파일 public 폴더 이동 및 JobFilter 스타일(CSS) 수정

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -6,21 +6,16 @@ import { Button } from '@mui/material';
 
 import './MainPage.css';
 
-import mainImage1 from '../assets/main_person1.png';
-import mainImage2 from '../assets/main_person2.png';
-import mainImage3 from '../assets/main_person3.png';
-import mainImage4 from '../assets/main_person4.png';
-
 function MainPage() {
   return (
     <div className="main-container">
       {/* 이미지 섹션 */}
       <section className="image-section">
         <div className="image-container">
-          <img src={mainImage1} alt="일러스트1" />
-          <img src={mainImage2} alt="일러스트2" />
-          <img src={mainImage3} alt="일러스트3" />
-          <img src={mainImage4} alt="일러스트4" />
+          <img src="/main_person1.png" alt="일러스트1" />
+          <img src="/main_person2.png" alt="일러스트2" />
+          <img src="/main_person3.png" alt="일러스트3" />
+          <img src="/main_person4.png" alt="일러스트4" />
         </div>
       </section>
 
@@ -28,10 +23,10 @@ function MainPage() {
       <section className="message-section">
         <h2>개발자들 요즘머함?</h2>
         <p>
-          자신에게 필요한 기술스택을 추천받고 싶으면 <br className="mobile-break"></br>계정을 만들거나
-          로그인하세요.
+          자신에게 필요한 기술스택을 추천받고 싶으면{' '}
+          <br className="mobile-break"></br>계정을 만들거나 로그인하세요.
         </p>
-        <Button 
+        <Button
           className="shake-button"
           component={Link}
           to="/resume"
